3f1c9d4e2563e1dd3876dd6b837162070ad9fe06
Danial Shokouhmanesh <danialshokouhmanesh@outlook.com>
2023-06-11 19:59:48 +0330

    some clean up and ocelot added

---
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -0,0 +1,21 @@
+using OcelotApiGw;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+        Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration((hostingContext, config) =>
+            {
+                config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json", true, true);
+            })
+           // .UseSerilog(SeriLogger.Configure)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });        
+}
